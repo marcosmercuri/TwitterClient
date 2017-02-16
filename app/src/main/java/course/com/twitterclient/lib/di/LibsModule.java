04e4a15e94efe563c35ab950c9635b329e58881b
Marcos Mercuri <marcosmercuri@gmail.com>
2017-02-16 22:59:14 +0100

    Logout in menu

---
+++ app/src/main/java/course/com/twitterclient/lib/di/LibsModule.java
@@ -0,0 +1,40 @@
+package course.com.twitterclient.lib.di;
+
+
+import android.support.v4.app.Fragment;
+
+import com.bumptech.glide.Glide;
+
+import javax.inject.Singleton;
+
+import course.com.twitterclient.lib.base.EventBus;
+import course.com.twitterclient.lib.base.GlideImageLoader;
+import course.com.twitterclient.lib.base.GreenRobotEventBus;
+import course.com.twitterclient.lib.base.ImageLoader;
+import dagger.Module;
+import dagger.Provides;
+
+@Module
+public class LibsModule {
+    private Fragment fragment;
+
+    @Provides
+    @Singleton
+    EventBus provideEventBust() {
+        return new GreenRobotEventBus(
+                org.greenrobot.eventbus.EventBus.getDefault()
+        );
+    }
+
+    @Provides
+    @Singleton
+    ImageLoader provideImageLoader(Fragment fragment) {
+        return new GlideImageLoader(Glide.with(fragment));
+    }
+
+    @Provides
+    @Singleton
+    Fragment provideFragment() {
+        return this.fragment;
+    }
+}
